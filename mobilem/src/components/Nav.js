fcfcef3c28da1c38054c19646a47df15e5befd55
Tomás Murphy <tomas.murphy.tm@gmail.com>
2022-08-15 19:21:14 -0300

    Segunda entrega

---
+++ mobilem/src/components/Nav.js
@@ -0,0 +1,46 @@
+function Nav() {
+    return (
+<nav id="menu" className="ms-md-2 mx-auto sombra wow animate__animated animate__fadeInUpBig animate__delay-2s animate__slower fixed-top cajaNav navbar-dark navbar navbar-expand-md">
+      <div className="burgerTop container-fluid mt-3 mt-md-0">
+        <div className="bloqueLogo" id="1">
+          <a className="sombra navbar-brand mx-auto" href="#menu">Mobilem</a>
+          </div>
+        <button
+          id="button"
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <i className="bi bi-flower1"></i>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#menu" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Productos
+          </a>
+          <ul className="dropdown-menu">
+            <li><a className="dropdown-item" href="#menu">Placares</a></li>
+            <li><a className="dropdown-item" href="#menu">Sillones</a></li>
+            <li><a className="dropdown-item" href="#menu">Sommiers</a></li>
+          </ul>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" aria-current="page" href="#menu">Nosotros</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#menu">Contacto</a>
+        </li>
+      </ul>         
+       </div>
+        </div>
+    </nav>        
+    );
+  }
+  
+  export default Nav;
+  
